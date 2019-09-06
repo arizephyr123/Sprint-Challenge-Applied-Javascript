6b3d09830d4a4f2fac09106910056ac0ff78299b
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-06 08:38:57 -0700

    Header Done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+/*
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data);
+    CreateCard(response);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+function CreateCard(obj) {
+    //create elements
+    const 
+    wholeCard = document.createElement("div"),
+    headline = document.createElement("div"),
+    author = document.createElement("div"),
+    imgCont = document.createElement("div"),
+    img = document.createElement("img"),
+    byAuthorSpan = document.createElement("span");
+
+//assign classes
+  wholeCard.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgCont.classList.add("img-container");
+
+  //set textContent/attributes
+  wholeCard 
+  headline 
+  author 
+  imgCont 
+  img 
+  byAuthorSpan 
+
+  //create structure
+  wholeCard.appendChild(headline);
+  wholeCard.appendChile(author);
+  author.appendChild(imgCont);
+  author.appendChild(byAuthorSpan);
+  imgCont.appendChild(img);
+
+  return wholeCard;
+}
+*/
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+//for reference
+// arr.forEach(item => {
+//     let listItems = document.createElement("li");
+//     listItems.textContent = item;
+//     listWrap.appendChild(listItems);
+//     return listWrap;
+//   });
+
+/*
+const topics = document.querySelector(".topics");
+console.log(topics);
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    //CreateCard(response);
+  })
+  .catch(error => {
+    console.log("The TABS data was not returned", error);
+  });
+function createTab() {
+    tab = document.createElement("div");
+    tab.classList.add("tab");
+    //tab.textContent = ("");
+
+    return tab
+}
+*/
